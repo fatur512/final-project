edfd93f534705f9f68ad9d46d8893d83f8e9092d
Faturahman <faturrahmau@gmail.com>
2024-11-30 18:10:02 +0700

    fix payment method 1

--- app/components/paymentmethod/page.js
+++ app/components/paymentmethod/page.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const PaymentMethodSelector = ({ paymentMethods, selectedPaymentMethod, onPaymentMethodChange }) => {
+const PaymentMethodSelector = ({ paymentMethods = [], selectedPaymentMethod, onPaymentMethodChange }) => {
   return (
     <div className="w-full">
       <label htmlFor="paymentMethod" className="block mb-2 text-lg font-medium text-gray-700">
@@ -13,11 +13,15 @@ const PaymentMethodSelector = ({ paymentMethods, selectedPaymentMethod, onPaymen
         className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
         <option value="">Select payment method</option>
-        {paymentMethods.map((paymentMethod) => (
-          <option key={paymentMethod.id} value={paymentMethod.id}>
-            {paymentMethod.name}
-          </option>
-        ))}
+        {paymentMethods.length > 0 ? (
+          paymentMethods.map((paymentMethod) => (
+            <option key={paymentMethod.id} value={paymentMethod.id}>
+              {paymentMethod.name}
+            </option>
+          ))
+        ) : (
+          <option disabled>No payment methods available</option>
+        )}
       </select>
     </div>
   );
