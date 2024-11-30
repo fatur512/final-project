b26ca2e798a1ebad9a282b1edbb02711f653914b
Faturahman <faturrahmau@gmail.com>
2024-11-30 18:18:06 +0700

    fix react-hot-toast

--- app/components/checkoutbutton/page.js
+++ app/components/checkoutbutton/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
 const CheckoutButton = ({ handleCheckout, selectedPaymentMethod }) => {
   return (
